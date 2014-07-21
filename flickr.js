e08fdb2418b1526418d217f4dd243757616c19cd
Brian Lonsdorf <brian@looprecur.com>
2014-07-21 09:50:19 -0700

    step1 cleaned up

--- flickr.js
+++ flickr.js
@@ -1,57 +1,30 @@
 /*jslint nomen: true */
 requirejs.config({
-  shim: {
-  },
+  shim: {},
   paths: {
     domReady: 'https://cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min',
     jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
     ramda: 'https://cdnjs.cloudflare.com/ajax/libs/ramda/0.2.3/ramda.min',
-    lambda: 'https://raw.githack.com/loop-recur/lambdajs/master/dist/lambda.amd',
-    pointfree: 'https://raw.githack.com/DrBoolean/pointfree-fantasy/master/dist/pointfree.amd',
-    bacon: 'https://cdnjs.cloudflare.com/ajax/libs/bacon.js/0.7.14/Bacon',
     future: 'http://looprecur.com/hostedjs/v2/data.future.umd',
-    future: 'http://looprecur.com/hostedjs/v2/data.future.umd',
-    io: 'http://looprecur.com/hostedjs/v2/io',
     maybe: 'http://looprecur.com/hostedjs/v2/maybe',
     id: 'http://looprecur.com/hostedjs/v2/id',
-    monoids: 'http://looprecur.com/hostedjs/v2/monoids',
-    either: 'http://looprecur.com/hostedjs/v2/data.either.umd',
-    string: 'http://looprecur.com/hostedjs/v2/string',
-    fn: 'http://looprecur.com/hostedjs/v2/function',
-    array: 'http://looprecur.com/hostedjs/v2/array'
+    hcjs: 'http://looprecur.com/hostedjs/v2/hcjs'
   }
 });
 
 require([
   'ramda',
   'jquery',
-  'lambda',
-  'pointfree',
   'future',
-  'bacon',
-  'io',
-  'monoids',
+  'hcjs',
   'domReady!'
 ],
-function (_, $, L, pf, Future, b, io, Monoids) {
-  io.extendFn();
-  L.expose(window);
-  pf.expose(window);
-  var getJSON  = function (url) {
-      return new Future(function(rej, res){
-        return $.getJSON(url,res);
-      });
-    },
-  listen      = _.curry(function(name, el) { return b.fromEventTarget(el, name); }),
-  onValue      = _.curry(function(f, s) { return s.onValue(f); }),
-  setHtml = _.curry(function($el, h){
-    $el.html(h);
-    return $el;
-  }),
-  log         = function(x){ console.log(x); return x };
+function (_, $, Future, hcjs) {
 
+  //  imageTag :: URL -> DOM
+  var imageTag = function (url) { return $('<img />', { src: url }); };
 
-  /////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////
   // Flickr api
 
   //  flickrFeed :: Future FlickrSearch
@@ -60,19 +33,16 @@ function (_, $, L, pf, Future, b, io, Monoids) {
   //  imageUrls :: FlickrSearch -> [URL]
   var imageUrls = compose(_.pluck('m'), _.pluck('media'), _.get('items'));
 
-  //  imageTag :: URL -> DOM
-  var imageTag = function (url) { return $('<img />', { src: url }); };
+  //  images :: FlickrSearch -> [DOM]
+  var images = compose(map(imageTag), imageUrls);
 
-  var makeImages = compose(map(imageTag), imageUrls);
-
-  //  widget :: _ -> Future DOM
+  //  widget :: Future [DOM]
   var widget = map(makeImages, flickrFeed);
 
 
   /////////////////////////////////////////////////////////////////////////////////////
   // Test code
 
-  widget.fork(log, setHtml($('#flickr1')));
-
+  widget.fork(log, setHtml($('#flickr')));
 });
 
