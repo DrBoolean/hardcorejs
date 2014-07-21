a7d277fc0ab64931e12bf57432b2da51a2fddb09
Brian Lonsdorf <brian@looprecur.com>
2014-07-21 16:00:45 -0700

    updated to do tags here

--- flickr.js
+++ flickr.js
@@ -6,8 +6,6 @@ requirejs.config({
     jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
     ramda: 'https://cdnjs.cloudflare.com/ajax/libs/ramda/0.2.3/ramda.min',
     future: 'http://looprecur.com/hostedjs/v2/data.future.umd',
-    maybe: 'http://looprecur.com/hostedjs/v2/maybe',
-    id: 'http://looprecur.com/hostedjs/v2/id',
     hcjs: 'http://looprecur.com/hostedjs/v2/hcjs'
   }
 });
@@ -21,8 +19,23 @@ require([
 ],
 function (_, $, Future, hcjs) {
 
+  //  imageTag :: URL -> DOM
+  var imageTag = function (url) { return $('<img />', { src: url }); };
+
+  //  aTag :: {count: Int, text: String} -> DOM
+  var aTag = function (t, c) { return $('<a />', { style: 'font-size:'+c+'em', html: t}); };
+
+  var countToP = function(counts) {
+    return Object.keys(counts).map(function(t) {
+      return aTag(t, counts[t]);
+    });
+  }
+
+  /////////////////////////////////////////////////////////////////////////////////////
+  // PictureBox
+
   //  PictureBox = data
-  //    val :: Future(a)
+  //    val :: Future([URL])
   var _PictureBox = function(val) {
     this.val = val;
     this.fork = this.val.fork;
@@ -31,52 +44,43 @@ function (_, $, Future, hcjs) {
   var PictureBox = function(x){ return new _PictureBox(x); }
 
   // instance Monoid PictureBox where
-  _PictureBox.prototype.empty = function () { return PictureBox(Future.of([])); };
+  _PictureBox.prototype.empty = function () { return PictureBox(Future.of(Tuple)); };
   _PictureBox.prototype.concat = function (y) {
     return PictureBox(this.val.concat(y.val));
   };
 
-
-  //  imageTag :: URL -> DOM
-  var imageTag = function (url) { return $('<img />', { src: url }); };
-
   /////////////////////////////////////////////////////////////////////////////////////
   // Flickr api
 
-  //  flickrFeed :: Future FlickrSearch
-  var flickrFeed = getJSON('http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&format=json&jsoncallback=?');
+  //  url :: String -> URL
+  var url = function(t) {
+    return 'http://api.flickr.com/services/feeds/photos_public.gne?tags='+t+'&format=json&jsoncallback=?';
+  };
 
-  //  imageUrls :: FlickrSearch -> [URL]
-  var imageUrls = compose(_.pluck('m'), _.pluck('media'), _.get('items'));
+  //  src :: FlickrItem -> URL
+  var src = compose(_.get('m'), _.get('media'));
+
+  //  srcs :: FlickrSearch -> [URL]
+  var srcs = compose(map(src), _.get('items'));
 
   //  images :: FlickrSearch -> [DOM]
-  var images = compose(map(imageTag), imageUrls);
+  var images = compose(map(imageTag), srcs);
+
+  //  tags :: FlickrSearch -> [DOM]
+  var tags = compose(countToP, _.countBy(_.identity), _.reject(_.isEmpty), chain(split(' ')), _.pluck('tags'), _.get('items'));
+
+  //  imagesAndTags :: Tuple [DOM] [DOM]
+  var imagesAndTags = liftA2(Tuple, images, tags)
 
   //  widget :: PictureBox
-  var widget = PictureBox(map(makeImages, flickrFeed));
+  var widget = compose(PictureBox, map(imagesAndTags), getJSON, url);
 
-
-  /////////////////////////////////////////////////////////////////////////////////////
-  // Youtube api
-
-  //  youtubeFeed :: Future YoutubeSearch
-  var youtubeFeed = getJSON('http://gdata.youtube.com/feeds/api/videos?q=cats&alt=json');
-
-  //  firstUrl :: [{url: String}] -> String
-  var firstUrl = compose(_.get('url'), _.first);
-
-  //  imageUrls :: YoutubeSearch -> [URL]
-  var imageUrls = compose(map(firstUrl), _.pluck('media$thumbnail'), _.pluck('media$group'), _.get('entry'), _.get('feed'));
-
-  //  images :: YoutubeSearch -> [DOM]
-  var images = compose(map(imageTag), imageUrls);
-
-  //  youtube_widget :: PictureBox
-  var youtube_widget = PictureBox(map(makeImages, youtubeFeed));
 
   /////////////////////////////////////////////////////////////////////////////////////
   // Test code
 
-  mconcat([youtube_widget, widget]).fork(log, setHtml($('#flickr')));
+  mconcat([widget('cats'), widget('dogs')]).fork(log, function(x){
+    compose(setHtml($('#flickr')), _.first)(x)
+    compose(setHtml($('#tagcloud')), _.get(1))(x)
+  });
 });
-
