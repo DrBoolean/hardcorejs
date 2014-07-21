ef1f977adf03a90c7da5b9bcce550604c63aee4e
Brian Lonsdorf <brian@looprecur.com>
2014-07-21 09:48:06 -0700

    cleanup step3

--- flickr.js
+++ flickr.js
@@ -1,63 +1,28 @@
 /*jslint nomen: true */
 requirejs.config({
-  shim: {
-  },
+  shim: {},
   paths: {
     domReady: 'https://cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min',
     jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
     ramda: 'https://cdnjs.cloudflare.com/ajax/libs/ramda/0.2.3/ramda.min',
-    lambda: 'https://raw.githack.com/loop-recur/lambdajs/master/dist/lambda.amd',
-    pointfree: 'https://raw.githack.com/DrBoolean/pointfree-fantasy/master/dist/pointfree.amd',
-    bacon: 'https://cdnjs.cloudflare.com/ajax/libs/bacon.js/0.7.14/Bacon',
     future: 'http://looprecur.com/hostedjs/v2/data.future.umd',
-    future: 'http://looprecur.com/hostedjs/v2/data.future.umd',
-    io: 'http://looprecur.com/hostedjs/v2/io',
     maybe: 'http://looprecur.com/hostedjs/v2/maybe',
     id: 'http://looprecur.com/hostedjs/v2/id',
-    monoids: 'http://looprecur.com/hostedjs/v2/monoids',
-    either: 'http://looprecur.com/hostedjs/v2/data.either.umd',
-    string: 'http://looprecur.com/hostedjs/v2/string',
-    fn: 'http://looprecur.com/hostedjs/v2/function',
-    array: 'http://looprecur.com/hostedjs/v2/array'
+    hcjs: 'http://looprecur.com/hostedjs/v2/hcjs'
   }
 });
 
 require([
   'ramda',
   'jquery',
-  'lambda',
-  'pointfree',
   'future',
-  'bacon',
-  'io',
-  'monoids',
+  'hcjs',
   'domReady!'
 ],
-function (_, $, L, pf, Future, b, io, Monoids) {
-  io.extendFn();
-  L.expose(window);
-  pf.expose(window);
-  var getJSON  = function (url) {
-      return new Future(function(rej, res){
-        return $.getJSON(url,res);
-      });
-    },
-  listen      = _.curry(function(name, el) { return b.fromEventTarget(el, name); }),
-  onValue      = _.curry(function(f, s) { return s.onValue(f); }),
-  setHtml = _.curry(function($el, h){
-    $el.html(h);
-    return $el;
-  }),
-  log         = function(x){ console.log(x); return x };
-
- Future.prototype.concat = function(x) {
-   return liftA2(concat, this, x);
- }
-  /////////////////////////////////////////////////////////////////////////////////////
-  // PictureBox
+function (_, $, Future, hcjs) {
 
   //  PictureBox = data
-  //    val :: Future([URL])
+  //    val :: Future(a)
   var _PictureBox = function(val) {
     this.val = val;
     this.fork = this.val.fork;
@@ -71,6 +36,10 @@ function (_, $, L, pf, Future, b, io, Monoids) {
     return PictureBox(this.val.concat(y.val));
   };
 
+
+  //  imageTag :: URL -> DOM
+  var imageTag = function (url) { return $('<img />', { src: url }); };
+
   /////////////////////////////////////////////////////////////////////////////////////
   // Flickr api
 
@@ -80,12 +49,10 @@ function (_, $, L, pf, Future, b, io, Monoids) {
   //  imageUrls :: FlickrSearch -> [URL]
   var imageUrls = compose(_.pluck('m'), _.pluck('media'), _.get('items'));
 
-  //  imageTag :: URL -> DOM
-  var imageTag = function (url) { return $('<img />', { src: url }); };
+  //  images :: FlickrSearch -> [DOM]
+  var images = compose(map(imageTag), imageUrls);
 
-  var makeImages = compose(map(imageTag), imageUrls);
-
-  //  widget :: Future DOM
+  //  widget :: PictureBox
   var widget = PictureBox(map(makeImages, flickrFeed));
 
 
@@ -101,17 +68,15 @@ function (_, $, L, pf, Future, b, io, Monoids) {
   //  imageUrls :: YoutubeSearch -> [URL]
   var imageUrls = compose(map(firstUrl), _.pluck('media$thumbnail'), _.pluck('media$group'), _.get('entry'), _.get('feed'));
 
-  //  imageTag :: URL -> DOM
-  var imageTag = function (url) { return $('<img />', { src: url }); };
+  //  images :: YoutubeSearch -> [DOM]
+  var images = compose(map(imageTag), imageUrls);
 
-  var makeImages = compose(map(imageTag), imageUrls);
-
-  //  youtube_widget :: Future DOM
+  //  youtube_widget :: PictureBox
   var youtube_widget = PictureBox(map(makeImages, youtubeFeed));
 
   /////////////////////////////////////////////////////////////////////////////////////
   // Test code
 
-  mconcat([youtube_widget, widget]).fork(log, setHtml($('#flickr1')));
+  mconcat([youtube_widget, widget]).fork(log, setHtml($('#flickr')));
 });
 
