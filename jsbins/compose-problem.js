04a1532f6d2b15333f341375f0433713e7811c63
Brian <brian@looprecur.com>
2014-07-23 12:44:53 -0700

    Update compose-problem.js

--- jsbins/compose-problem.js
+++ jsbins/compose-problem.js
@@ -1,5 +1,6 @@
 requirejs.config({
-  shim: {},
+  shim: {
+  },
   paths: {
     domReady: 'https://cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min',
     hcjs: 'http://looprecur.com/hostedjs/v2/hcjs',
@@ -14,110 +15,124 @@ require(
     'domReady!'
   ],
   function (_) {
-    console.clear();
+  console.clear();
+    
+/******************************************
+    C O M P O S I T I O N  E X A M P L E
+******************************************/
+
+// Curried functions are easy to compose.
+// Using _.map, _.size, and _.split we can
+// make a function that returns the lengths
+// of the words in a string.
+
+var lengths = _.compose(
+  _.map(_.size), _.split(' ')
+);
+console.log(lengths('once upon a time'));
+
+/*******************************************
+               Y O U R  T U R N
+********************************************/
+
+var articles = [
+  {
+    title: 'Everything Sucks',
+    url: 'http://do.wn/sucks.html',
+    author: {
+      name: 'Debbie Downer',
+      email: 'debbie@do.wn'
+    }
+  },
+  {
+    title: 'If You Please',
+    url: 'http://www.geocities.com/milq',
+    author: {
+      name: 'Caspar Milquetoast',
+      email: 'hello@me.com'
+    }
+  }
+];
+    
+
+console.log("--------Start exercise 1--------");
+// -- Challenge 1 -------------------------
+// Write a function to return the first title of the
+// articles above using only _.get, _.head, and
+// _.compose.
+    
+    
+var firstTitle = undefined;
+    
+assertEqual('Everything Sucks', firstTitle(articles));
+
+console.log("--------Exercise 1 pass!--------");
+
+    
+    
+console.log("--------Start exercise 2--------");
+// -- Challenge 2 -------------------------
+// Return a list of the author names in
+// articles using only _.get, _.compose, and
+// _.map.
+
+var names = undefined;
+assertEqual(
+  ['Debbie Downer', 'Caspar Milquetoast'],
+  names(articles)
+);
+    
+console.log("--------Exercise 2 pass!--------");
 
 
+    
+console.log("--------Start exercise 3--------");
+// -- Challenge 3 -------------------------
+// isAuthor uses the names function above to see if
+// a given person wrote any of the articles.
+// "Uncompose" the function below by rewrite it
+// without _.compose to make it more natural.
 
-    /******************************************
-        C O M P O S I T I O N  E X A M P L E
-    ******************************************/
+var isAuthor = _.curry(function(x, xs){
+  return _.compose(_.contains(x), names)(xs);
+});
 
-    // Curried functions are easy to compose.
-    // Using _.map, _.size, and _.split we can
-    // make a function that returns the lengths
-    // of the words in a string.
+assertEqual(
+  false,
+  isAuthor('New Guy', articles)
+);
+assertEqual(
+  true,
+  isAuthor('Debbie Downer', articles)
+);
 
-    var lengths = _.compose(
-      _.map(_.size), _.split(' ')
-    );
-    console.log(lengths('once upon a time'));
+console.log("--------Exercise 3 pass!--------");
+    
+    
+    
 
+console.log("--------Start exercise 4--------");
+// -- Challenge 4 -------------------------
+// There is more to point-free programming
+// than compose! Let's build ourselves
+// another function that combines functions
+// to let us write code without glue variables.
 
+var fork = _.curry(function(lastly, f, g, x) {
+  return lastly(f(x), g(x));
+});
 
+// As you can see, the fork function is a
+// pipeline like compose, except it duplicates
+// its value, sends it to two functions, then
+// sends the results to a combining function.
+//
+// Your challenge: implement a function to
+// compute the average values in a list using
+// only fork, _.divide, _.sum, and _.size.
 
-    /*******************************************
-                   Y O U R  T U R N
-    ********************************************/
+var avg = undefined; // change this
+assertEqual(3, avg([1,2,3,4,5]));
+console.log("--------Exercise 4 pass!--------");
 
-    var articles = [{
-      title: 'Everything Sucks',
-      url: 'http://do.wn/sucks.html',
-      author: {
-        name: 'Debbie Downer',
-        email: 'debbie@do.wn'
-      }
-    }, {
-      title: 'If You Please',
-      url: 'http://www.geocities.com/milq',
-      author: {
-        name: 'Caspar Milquetoast',
-        email: 'hello@me.com'
-      }
-    }];
-
-    console.log("--------Start exercise 1--------");
-    // -- Challenge 1 -------------------------
-    // Return a list of the author names in
-    // articles using only get, _.compose, and
-    // _.map.
-
-    var names = undefined;
-
-    assertEqual(
-      ['Debbie Downer', 'Caspar Milquetoast'],
-      names(articles)
-    );
-
-    console.log("--------Exercise 1 pass!--------");
-
-
-
-
-    console.log("--------Start exercise 2--------");
-    // -- Challenge 2 -------------------------
-    // Make a boolean function that says whether
-    // a given person wrote any of the articles.
-    // Use the names function you wrote above
-    // with _.compose and _.contains.
-
-    var isAuthor = undefined;
-
-    assertEqual(
-      false,
-      isAuthor('New Guy', articles)
-    );
-    assertEqual(
-      true,
-      isAuthor('Debbie Downer', articles)
-    );
-
-    console.log("--------Exercise 2 pass!--------");
-
-
-
-
-    console.log("--------Start exercise 3--------");
-    // -- Challenge 3 -------------------------
-    // There is more to point-free programming
-    // than compose! Let's build ourselves
-    // another function that combines functions
-    // to let us write code without glue variables.
-
-    var fork = _.curry(function (lastly, f, g, x) {
-      return lastly(f(x), g(x));
-    });
-
-    // As you can see, the fork function is a
-    // pipeline like compose, except it duplicates
-    // its value, sends it to two functions, then
-    // sends the results to a combining function.
-    //
-    // Your challenge: implement a function to
-    // compute the average values in a list using
-    // only fork, _.divide, _.sum, and _.size.
-
-    var avg = undefined; // change this
-    assertEqual(3, avg([1, 2, 3, 4, 5]));
-    console.log("--------Exercise 3 pass!--------");
-
-  });
+})
