dff8777e2df0b819e429cb3bc4870282ec747e15
Joe Nelson <cred+github@begriffs.com>
2014-07-23 20:55:29 -0700

    Starter files for in-class demo

---
+++ demos/step1/youtube-base.js
@@ -0,0 +1,30 @@
+/*jslint nomen: true */
+requirejs.config({
+  shim: {},
+  paths: {
+    domReady: 'https://cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min',
+    jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
+    ramda: 'https://cdnjs.cloudflare.com/ajax/libs/ramda/0.2.3/ramda.min',
+    future: 'http://looprecur.com/hostedjs/v2/data.future.umd',
+    hcjs: 'http://looprecur.com/hostedjs/v2/hcjs'
+  }
+});
+
+require([
+    'ramda',
+    'jquery',
+    'future',
+    'hcjs',
+    'domReady!'
+  ],
+  function (_, $, Future, hcjs) {
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    // Youtube api
+
+    //  url :: String -> URL
+    var url = function (t) {
+      return 'http://gdata.youtube.com/feeds/api/videos?q=' + t + '&alt=json';
+    };
+
+  });
