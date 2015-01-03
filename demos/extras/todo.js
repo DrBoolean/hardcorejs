5ab57fb03dc8773ec4b696f934d49b045df3ed0b
Brian Lonsdorf <brian@looprecur.com>
2015-01-03 11:26:14 -0800

    todo update

--- demos/extras/todo.js
+++ demos/extras/todo.js
@@ -3,77 +3,82 @@ requirejs.config({
   shim: {},
   paths: {
     domReady: 'https://cdnjs.cloudflare.com/ajax/libs/require-domReady/2.0.1/domReady.min',
-    handlebars: '//cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0-alpha.4/handlebars.amd',
-    jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
+    handlebars: 'https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.amd.min',
     ramda: 'https://cdnjs.cloudflare.com/ajax/libs/ramda/0.2.3/ramda.min',
-    lambda: 'https://raw.githack.com/loop-recur/lambdajs/master/dist/lambda.amd',
-    pointfree: 'https://raw.githack.com/DrBoolean/pointfree-fantasy/master/dist/pointfree.amd',
     bacon: 'https://cdnjs.cloudflare.com/ajax/libs/bacon.js/0.7.14/Bacon',
-    future: 'http://looprecur.com/hostedjs/v2/data.future.umd',
-    maybe: 'http://looprecur.com/hostedjs/v2/maybe',
-    id: 'http://looprecur.com/hostedjs/v2/id',
-    monoids: 'http://looprecur.com/hostedjs/v2/monoids',
-    either: 'http://looprecur.com/hostedjs/v2/data.either.umd',
-    string: 'http://looprecur.com/hostedjs/v2/string',
-    fn: 'http://looprecur.com/hostedjs/v2/function',
-    array: 'http://looprecur.com/hostedjs/v2/array'
+    hcjs: 'http://looprecur.com/hostedjs/v2/hcjs',
+    daggy: 'daggy',
+    reader: 'reader',
+    maybe: 'data.maybe.umd'
   }
 });
 
 require([
     'ramda',
     'handlebars',
-    'lambda',
-    'pointfree',
-    'future',
     'bacon',
-    'monoids',
+    'hcjs',
+    'reader',
+    'maybe',
     'domReady!'
   ],
-  function (_, H, L, pf, Future, b, Monoids) {
-    // {{{ setup
-    L.expose(window);
-    pf.expose(window);
+  function (_, H, b, hcjs, Reader, Maybe) {
+
+    // generic helpers
+    //////////////////////////////////////////////////////////////////////////////
     var Handlebars = H.default,
+
       targetValue = compose(_.get('value'), _.get('target')),
-      listen = _.curry(function (name, el) {
-        return b.fromEventTarget(el, name);
-      }),
+
+      getOrElse = _.curry(function(x, m){ return m.getOrElse(x); }),
+
       $ = function (sel) {
         return document.querySelector(sel);
-      },
-      setHtml = _.curry(function (sel, h) {
-        $(sel).innerHTML = h;
-      }),
-      getFromStorage = function (name) {
-        return JSON.parse(localStorage[name]);
-      },
-      saveToStorage = _.curry(function (name, val) {
-        localStorage[name] = JSON.stringify(val);
-        return val;
-      }),
-      log = function (x) {
-        console.log(x);
-        return x;
-      };
-    // }}}
+      }.toIO(),
 
+      setHtml = _.curry(function (sel, h) {
+        return $(sel).map(function(s) { s.innerHTML = h; });
+      }),
+
+      getFromStorage = function (name) { return Maybe.fromNullable(localStorage[name]) }.toIO(),
+
+      saveToStorage = function (name) {
+        return function(val) {
+          localStorage[name] = JSON.stringify(val);
+          return val;
+        }.toIO()
+      };
+
+
+    // pure app
     //////////////////////////////////////////////////////////////////////////////
 
     var isEnterKey = compose(eq(13), _.get('keyCode')),
-      textOnEnter = compose(map(targetValue), filter(isEnterKey), listen('keyup')),
-      renderTodos = Handlebars.compile($('#todo-tpl').innerHTML),
-      updatePage = compose(setHtml('#main'), renderTodos),
 
-      appendTodo = function (t) {
-        return unshift(t, getFromStorage('todos'));
-      },
-      renderTodos = compose(updatePage, getFromStorage),
-      persistTodo = compose(saveToStorage('todos'), appendTodo),
-      saveTodo = compose(updatePage, persistTodo);
+      updatePage = setHtml('#main'),
 
+      getTodos = getFromStorage("todos").map(getOrElse("[]")).map(JSON.parse),
+
+      appendTodo = compose(getTodos.map.bind(getTodos), unshift)
+
+      persistTodo = compose(chain(saveToStorage('todos')), appendTodo),
+
+      saveTodo = Reader.ask.map(function (render) {
+        return compose(chain(updatePage), map(render), persistTodo);
+      });
+
+
+    // impure calling code
     //////////////////////////////////////////////////////////////////////////////
 
-    textOnEnter($('input')).onValue(saveTodo);
-    renderTodos('todos');
-  });
+    var render = Handlebars.compile($('#todo-tpl').runIO().innerHTML);
+    var input = _.head(document.getElementsByTagName('input'));
+
+    b.fromEventTarget(input, 'keyup').filter(isEnterKey).map(targetValue).onValue(function(t){
+      saveTodo.run(render)(t).runIO();
+    });
+
+    getTodos.map(render).chain(updatePage).runIO();
+ }
+);
+
